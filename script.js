61a242aa56a158bb4a840acdff8c5b059c29b209
dyonak <dyonak@gmail.com>
2023-12-07 11:37:06 -0600

    Script exclude on last update for some reason.

--- script.js
+++ script.js
@@ -27,6 +27,29 @@ sizeSlider.addEventListener('mouseup', (e) => {
     fillBlocks();
 });
 
+let numColors = 100;
+
+function rainbow(numOfSteps, step) {
+    // This function generates vibrant, "evenly spaced" colours (i.e. no clustering). This is ideal for creating easily distinguishable vibrant markers in Google Maps and other apps.
+    // Adam Cole, 2011-Sept-14
+    // HSV to RBG adapted from: http://mjijackson.com/2008/02/rgb-to-hsl-and-rgb-to-hsv-color-model-conversion-algorithms-in-javascript
+    var r, g, b;
+    var h = step / numOfSteps;
+    var i = ~~(h * 6);
+    var f = h * 6 - i;
+    var q = 1 - f;
+    switch(i % 6){
+        case 0: r = 1; g = f; b = 0; break;
+        case 1: r = q; g = 1; b = 0; break;
+        case 2: r = 0; g = 1; b = f; break;
+        case 3: r = 0; g = q; b = 1; break;
+        case 4: r = f; g = 0; b = 1; break;
+        case 5: r = 1; g = 0; b = q; break;
+    }
+    var c = "#" + ("00" + (~ ~(r * 255)).toString(16)).slice(-2) + ("00" + (~ ~(g * 255)).toString(16)).slice(-2) + ("00" + (~ ~(b * 255)).toString(16)).slice(-2);
+    return (c);
+}
+
 function fillBlockRow(blockRow, i) {
     while (i) {
         let block = document.createElement('div');
@@ -34,10 +57,10 @@ function fillBlockRow(blockRow, i) {
         block.style.width = blockSize + 'px';
         block.style.height = blockSize + 'px';
         block.addEventListener('mouseover', function(e) {
-            if (currentColorIndex >= RAINBOW.length) {
+            if (currentColorIndex >= numColors) {
                 currentColorIndex = 0;
             }
-            e.target.style.backgroundColor = RAINBOW[currentColorIndex];
+            e.target.style.backgroundColor = rainbow(numColors, currentColorIndex);
             currentColorIndex++;
         });
         blockRow.appendChild(block);
