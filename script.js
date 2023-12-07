585e0bdfe16885c61361a9a7c2879f728aa22e9a
dyonak <dyonak@gmail.com>
2023-12-07 11:30:05 -0600

    Rainbow effect and block size adjustment added
    -Block coloring will now cycle between an array of rainbow colors
    -Block size can be adjusted, with enforcement for "safe" sizes that don't break the block creation script

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
 const GRIDSIZE = 960;
+const RAINBOW = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+let currentColorIndex = 0;
 let blockSize = 32;
 
 gridContainer = document.querySelector('.etchasketchbox');
+sizeSlider = document.querySelector('.slider');
+
+sizeSlider.addEventListener('mouseup', (e) => {
+
+    blockRows = document.querySelectorAll('.blockRow');
+    blockRows.forEach(element => {
+        element.remove();
+    });
+
+    console.log(`Checking ${e.target.value} and ${GRIDSIZE}`);
+    
+    blockSize = e.target.value;
+
+    while (GRIDSIZE % blockSize) {
+        blockSize++;
+    }
+
+    console.log(`Set block size to ${blockSize}`);
+    e.target.value = blockSize;
+    fillBlocks();
+});
 
 function fillBlockRow(blockRow, i) {
     while (i) {
@@ -10,16 +34,25 @@ function fillBlockRow(blockRow, i) {
         block.style.width = blockSize + 'px';
         block.style.height = blockSize + 'px';
         block.addEventListener('mouseover', function(e) {
-            e.target.style.backgroundColor = "darkgrey";
+            if (currentColorIndex >= RAINBOW.length) {
+                currentColorIndex = 0;
+            }
+            e.target.style.backgroundColor = RAINBOW[currentColorIndex];
+            currentColorIndex++;
         });
         blockRow.appendChild(block);
         i--;
     };
 };
 
-while (document.querySelectorAll(".blockRow").length < GRIDSIZE / blockSize){
-    blockRow = document.createElement('div');
-    blockRow.className = 'blockRow';
-    gridContainer.appendChild(blockRow);
-    fillBlockRow(blockRow, GRIDSIZE / blockSize);
+function fillBlocks() {
+    while (document.querySelectorAll(".blockRow").length < GRIDSIZE / blockSize){
+        blockRow = document.createElement('div');
+        blockRow.className = 'blockRow';
+        gridContainer.appendChild(blockRow);
+        fillBlockRow(blockRow, GRIDSIZE / blockSize);
+        
+    };
 };
+
+fillBlocks();
